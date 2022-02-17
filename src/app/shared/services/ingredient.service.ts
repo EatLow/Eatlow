06613724a9927b26c6ebc3be9b0b476f1b3669be
Pertoldi <antoine.pertoldi@gmail.com>
2022-02-17 15:49:14 +0100

    changement de branch pb routing

--- src/app/shared/services/ingredient.service.ts
+++ src/app/shared/services/ingredient.service.ts
@@ -1,19 +1,31 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Ingredients } from '../models/ingredient/ingredient';
 
 @Injectable({
   providedIn: 'root'
 })
 export class IngredientService {
 
+  private urlApiIngredients = environment.urlApi + '/api/ingredients';
+
+  ingredients$ = new BehaviorSubject<Ingredients[]>([]);
+
   constructor(private http: HttpClient) { }
 
   getAllIngredient() {
-
+    this.http.get<Ingredients[]>(this.urlApiIngredients + '/all').subscribe((ingredients: Ingredients[]) => {
+      this.ingredients$.next(ingredients)
+    })
   }
 
-  getOneIngredient() {
-
+  getOneIngredient(id: number) {
+    const ingredient = this.ingredients$.value.filter((i) => {
+      return i.id ? i.id === id : false;
+    })[0];
+    return { ...ingredient };
   }
 
 }
