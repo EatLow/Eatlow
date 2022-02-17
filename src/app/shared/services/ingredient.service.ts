65e1edc46fe3cfcb47bf20b1ee530322aadb5e6f
Pertoldi <antoine.pertoldi@gmail.com>
2022-02-17 11:59:16 +0100

    change branch

---
+++ src/app/shared/services/ingredient.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class IngredientService {
+
+  constructor(private http: HttpClient) { }
+
+  getAllIngredient() {
+
+  }
+
+  getOneIngredient() {
+
+  }
+
+}
