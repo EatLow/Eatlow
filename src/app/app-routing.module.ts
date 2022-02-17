ade1bf5c7309d2452c4eaffc41c2e17a787cf8ca
EnzoC <enzo.12.juin@gmail.com>
2022-02-17 18:52:53 +0100

    Mise en place de la page d'accueil
    Voir titre

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AccueilComponent } from './pages/accueil/accueil.component';
 
 
 const routes: Routes = [
-	{ path: 'test', loadChildren: () => import('./pages/pageExemple/page-exemple.module').then(m => m.PageExempleModule) }
+	{ path: 'test', loadChildren: () => import('./pages/pageExemple/page-exemple.module').then(m => m.PageExempleModule) },
+  { path: 'acc', component: AccueilComponent}
 ];
 
 @NgModule({
