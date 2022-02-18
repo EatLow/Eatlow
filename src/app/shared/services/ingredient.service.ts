2f4726b68beffd2cc8a13970e12a03cffb3e21bc
Pertoldi <antoine.pertoldi@gmail.com>
2022-02-18 10:17:50 +0100

    switch brnach to help on router

--- src/app/shared/services/ingredient.service.ts
+++ src/app/shared/services/ingredient.service.ts
@@ -1,5 +1,5 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { Injectable, OnInit } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { Ingredients } from '../models/ingredient/ingredient';
@@ -7,17 +7,22 @@ import { Ingredients } from '../models/ingredient/ingredient';
 @Injectable({
   providedIn: 'root'
 })
-export class IngredientService {
+export class IngredientService implements OnInit {
 
   private urlApiIngredients = environment.urlApi + '/api/ingredients';
 
   ingredients$ = new BehaviorSubject<Ingredients[]>([]);
+  ingredientsByName$ = new BehaviorSubject<Ingredients[]>([]);
 
   constructor(private http: HttpClient) { }
 
-  getAllIngredient() {
-    this.http.get<Ingredients[]>(this.urlApiIngredients + '/all').subscribe((ingredients: Ingredients[]) => {
-      this.ingredients$.next(ingredients)
+  ngOnInit() {
+    this.getAllIngredients();
+  }
+
+  getAllIngredients() {
+    this.http.get<Ingredients[]>(`${this.urlApiIngredients}/all`).subscribe((ingredients: Ingredients[]) => {
+      this.ingredients$.next(ingredients);
     })
   }
 
@@ -26,6 +31,12 @@ export class IngredientService {
       return i.id ? i.id === id : false;
     })[0];
     return { ...ingredient };
+  }
+
+  getIngredientsByName(name: string) {
+    this.http.get<Ingredients[]>(`${this.urlApiIngredients}/search/${name}`).subscribe((ingredients: Ingredients[]) => {
+      this.ingredientsByName$.next(ingredients);
+    })
   }
 
 }
---
+++ src/app/shared/services/meal.service.ts
@@ -0,0 +1,41 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Meal } from '../models/meal/meal';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MealService {
+
+  private urlApiIngredients = environment.urlApi + '/api/ingredients';
+
+  meals$ = new BehaviorSubject<Meal[]>([]);
+  mealsByName$ = new BehaviorSubject<Meal[]>([]);
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.getAllMeals();
+  }
+
+  getAllMeals() {
+    this.http.get<Meal[]>(`${this.urlApiIngredients}/all`).subscribe((ingredients: Meal[]) => {
+      this.meals$.next(ingredients);
+    })
+  }
+
+  getOneIngredient(id: number) {
+    const ingredient = this.meals$.value.filter((i) => {
+      return i.id ? i.id === id : false;
+    })[0];
+    return { ...ingredient };
+  }
+
+  getIngredientsByName(name: string) {
+    this.http.get<Meal[]>(`${this.urlApiIngredients}/search/${name}`).subscribe((ingredients: Meal[]) => {
+      this.mealsByName$.next(ingredients);
+    })
+  }
+}
