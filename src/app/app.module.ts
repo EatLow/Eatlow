8e45fdc05c771caf6583362dd4328f987645102d
Pertoldi <antoine.pertoldi@gmail.com>
2022-02-18 17:04:17 +0100

    gestion conflict

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
