a17ac2c628d5096b6915c04eca97ac2df092cb77
Doriane <91633041+DorianeRos@users.noreply.github.com>
2022-02-21 15:43:45 +0100

    Header component (#3)
    * create navbar headerComponent
    
    * Finalisation du headerComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from './shared/components/header/header.component';
 
 
 const routes: Routes = [
