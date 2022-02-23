d4ac4a7ce781039baf3a9629018b9a8a9d6cd531
Boris Ahumada <boris.ahumada@laposte.net>
2022-02-23 15:54:03 +0100

    Implementing connexion buttons

--- src/app/pages/accueil/components/logging-buttons/logging-buttons.component.ts
+++ src/app/pages/accueil/components/logging-buttons/logging-buttons.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
+import { AuthService } from '../../../../shared/services/auth.service';
+
 @Component({
   selector: 'accueil-logging-buttons',
   templateUrl: './logging-buttons.component.html',
   styleUrls: ['./logging-buttons.component.scss']
 })
-export class LoggingButtonsComponent implements OnInit {
+export class LoggingButtonsComponent implements OnInit
+{
 
-  constructor() { }
+  public isAuth: boolean = false;
 
-  ngOnInit(): void {
+  constructor(private _authService: AuthService) { }
+
+  ngOnInit(): void
+  {
+    this._authService.isAuth$.subscribe((isAuth: boolean) =>
+    {
+      this.isAuth = isAuth;
+    })
   }
 
 }
