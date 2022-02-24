d4c1504b475a0f4a2f2317ffe92b758f9e6a4985
Pertoldi <antoine.pertoldi@gmail.com>
2022-02-24 17:46:46 +0100

    russie - ukraine

--- src/app/pages/plat/components/ingredient-detail/ingredient-detail.component.ts
+++ src/app/pages/plat/components/ingredient-detail/ingredient-detail.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IngredientDetailComponent implements OnInit {
 
+  ecoscore = 0.11;
+  title = "test"; 
+
   constructor() { }
 
   ngOnInit(): void {
