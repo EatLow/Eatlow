edcf617b624d0ec1b4e06e8c2817122ef66c5ead
Pertoldi <antoine.pertoldi@gmail.com>
2022-03-01 11:32:11 +0100

    modal as a component (#17)

---
+++ src/app/shared/components/modal/modal.component.ts
@@ -0,0 +1,21 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-modal',
+  templateUrl: './modal.component.html',
+  styleUrls: ['./modal.component.scss']
+})
+export class ModalComponent implements OnInit {
+
+  @Input()
+  title: string = '';
+
+  @Input()
+  content: string[] = [];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
