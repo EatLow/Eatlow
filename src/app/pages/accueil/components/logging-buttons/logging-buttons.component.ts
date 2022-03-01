d42c2d3a0bad67d5f09156b04b46f3362c1435ae
Pertoldi <antoine.pertoldi@gmail.com>
2022-03-01 19:58:12 +0100

    fix css acceuil + add header s routes

--- src/app/pages/accueil/components/logging-buttons/logging-buttons.component.ts
+++ src/app/pages/accueil/components/logging-buttons/logging-buttons.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { AuthService } from '../../../../shared/services/auth.service';
 
